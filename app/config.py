5ce51e4b20c4dfbb5275c61c932f34be88b5e10b
SimeonKillemaes <Simeon.killemaes@gmail.com>
2024-11-08 14:43:08 +0100

    commit config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-class Config: 
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+class Config:
+    SECRET_KEY = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.romqopuorvxndfjkwnqw:Freddy&Seppe123@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'      
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
