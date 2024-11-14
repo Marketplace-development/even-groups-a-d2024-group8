cdedb8936941a1872b1f51e0b637790f7ed4ac98
SophieHoeckman <hoeckman.sophie@gmail.com>
2024-11-14 13:38:32 +0100

    routes change

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,26 @@
 # app/routes.py
 
 from flask import Blueprint, request, redirect, url_for, render_template, session
-from .models import db, User
+from .models import db, Profile
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     if 'user_id' in session:
-        user = User.query.get(session['user_id'])
-        return render_template('index.html', username=user.username)
+        user = Profile.query.get(session['user_id'])
+        return render_template('index.html', username=user.name)
     return render_template('index.html', username=None)
 
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
-        if User.query.filter_by(username=username).first() is None:
-            new_user = User(username=username)
+        if Profile.query.filter_by(name=username).first() is None:
+            new_user = Profile(name=username)
             db.session.add(new_user)
             db.session.commit()
-            session['user_id'] = new_user.id
+            session['user_id'] = new_user.profile_id
             return redirect(url_for('main.index'))
         return 'Username already registered'
     return render_template('register.html')
@@ -29,9 +29,9 @@ def register():
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        user = User.query.filter_by(username=username).first()
+        user = Profile.query.filter_by(name=username).first()
         if user:
-            session['user_id'] = user.id
+            session['user_id'] = user.profile_id
             return redirect(url_for('main.index'))
         return 'User not found'
     return render_template('login.html')
